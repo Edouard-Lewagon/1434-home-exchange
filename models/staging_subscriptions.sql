a2223c0ffa2f6f184437f349182694120dfb1213
ed.deperiers@gmail.com <ed.deperiers@gmail.com>
2023-12-04 17:19:43 +0000

    t

--- models/staging_subscriptions.sql
+++ models/staging_subscriptions.sql
@@ -1,10 +1,18 @@
 -- models/occurrences_by_country.sql
 WITH country_occurrences AS (
-  SELECT country, COUNT(*) AS occurrences
-  FROM Home_exchange.subscriptions
-  GROUP BY country
+  SELECT
+    c.country,
+    COUNT(*) AS occurrences
+  FROM {{ source('home-exchange-407110', 'subscriptions') }} AS c
+  WHERE c.city IS NOT NULL OR c.region IS NOT NULL OR c.country IS NOT NULL OR c.department IS NOT NULL
+  GROUP BY c.country
 )
-SELECT *
-FROM country_occurrences
-ORDER BY occurrences DESC
+
+SELECT
+  s.*,
+  co.occurrences
+FROM {{ source('home-exchange-407110', 'subscriptions') }} AS s
+LEFT JOIN country_occurrences AS co
+ON s.country = co.country
+ORDER BY co.occurrences DESC
 LIMIT 11;
