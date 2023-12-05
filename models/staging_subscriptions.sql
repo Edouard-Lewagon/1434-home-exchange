a0603095d6f025ed08c8ec0d6537de33e390a2b8
Clément Lebas <clement.vestafinance@gmail.com>
2023-12-05 11:18:13 +0000

    cleaning v2

--- models/staging_subscriptions.sql
+++ models/staging_subscriptions.sql
@@ -2,16 +2,9 @@ WITH country_occurrences AS (
   SELECT country, COUNT(*) AS occurrences
   FROM Home_exchange.subscriptions
   GROUP BY country
-),
-
-unique_users AS (
-  SELECT
-    user_id,
-    ROW_NUMBER() OVER (PARTITION BY user_id) AS row_num
-  FROM Home_exchange.subscriptions
 )
-
-SELECT
+SELECT DISTINCT
+CONCAT(SUBSTR(CAST(subscription_date AS STRING), 1, 4), '_', CAST(user_id AS STRING)) AS annee_user_id,
 CAST(subscription_date AS DATE) AS subscription_date_,
 user_id,
 renew,
@@ -24,17 +17,11 @@ payment2,
 payment3,
 country,
 FROM Home_exchange.subscriptions
-WHERE 
+WHERE
   (country IS NOT NULL AND country IN (
     SELECT country
     FROM country_occurrences
     ORDER BY occurrences DESC
     LIMIT 11
-  ))
-  AND 
-  (user_id IS NOT NULL AND user_id IN (
-    SELECT user_id
-    FROM unique_users
-    WHERE row_num = 1
   ))
 
