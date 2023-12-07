af32d6fa14fa102751daf016d1a6f79c42f174bc
charline.buche@gmail.com <charline.buche@gmail.com>
2023-12-07 10:31:35 +0000

    changement top 3 pays

--- models/staging/staging_subscriptions.sql
+++ models/staging/staging_subscriptions.sql
@@ -1,8 +1,3 @@
-WITH country_occurrences AS (
-  SELECT country, COUNT(*) AS occurrences
-  FROM Home_exchange.subscriptions
-  GROUP BY country
-)
 SELECT DISTINCT
 CONCAT(SUBSTR(CAST(subscription_date AS STRING), 1, 4), '_', CAST(user_id AS STRING)) AS annee_user_id,
 CAST(subscription_date AS DATE) AS subscription_date_,
@@ -17,10 +12,4 @@ payment2,
 payment3,
 country,
 FROM Home_exchange.subscriptions
-WHERE
-  (country IS NOT NULL AND country IN (
-    SELECT country
-    FROM country_occurrences
-    ORDER BY occurrences DESC
-    LIMIT 11
-  ))
+WHERE country IN ('FRA', 'ESP', 'USA')
